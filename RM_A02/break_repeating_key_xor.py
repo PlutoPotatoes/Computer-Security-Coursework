60ee4e87b4b75a97b3a0c45cc8895ef0ed7afcb6
{PlutoPotatoes} <{ryan.morrell@gmail.com}>
2026-01-30 10:52:08 -0800

    started A02

--- RM_A02/break_repeating_key_xor.py
+++ RM_A02/break_repeating_key_xor.py
@@ -14,17 +14,22 @@ from single_byte_xor import crack_single_byte_xor
 from util import base64_to_bytearray, bytearray_to_str, str_to_bytearray
 
 from xor import extend_key, xor
+from numpy import mean
 
 
 def hamming_dist(a: bytearray | str, b: bytearray | str) -> int:
     """Compute the Hamming distance (number of diff bits) between strings."""
+    dist = 0
     assert (len(a) == len(b))
     if isinstance(a, str):
         a = str_to_bytearray(a)
     if isinstance(b, str):
         b = str_to_bytearray(b)
-    #TODO
-    return None
+    x = xor(a, b)
+    for bit in x:
+        dist+=bit.bit_count()
+
+    return dist
 
 
 def discover_block_size(
@@ -33,14 +38,38 @@ def discover_block_size(
             blocks_to_compare: int = 20,
         ):
     """Guess the most likely length of the unknown key."""
-    #TODO
-    return block_size
-
+    '''
+        The smallest hamming distance between sections of the text with a specific length is likely the key
+    '''
+    best = []
+    for KEY_LENGTH in range(1,max_block_size):
+        scores = []
+        for index in range(0,len(byte_array), KEY_LENGTH):
+            if index + 2*KEY_LENGTH > byte_array.__len__():
+                break
+            A = byte_array[index:index + KEY_LENGTH]
+            B = byte_array[index + KEY_LENGTH:index + (2*KEY_LENGTH)]
+            scores.append(hamming_dist(A,B)/(KEY_LENGTH* 4))
+        mean_score = mean(scores)
+        if(len(best) ==0):
+            best.append((KEY_LENGTH, mean_score))
+        for i in range(0, len(best)):
+            if(best[i][1] > mean_score):
+                best.insert(i,(KEY_LENGTH, mean_score))
+                if(len(best) > 3):
+                    best.pop()
+                break
+    return best
 
 def crack_repeating_key_xor(encrypted: bytearray) -> str:
     """Extract an unknown key from a given crypttext."""
-    block_size = discover_block_size(encrypted, blocks_to_compare=15)
+    block_size = discover_block_size(encrypted, blocks_to_compare=15)[0][0]
+    key = ''
     #TODO
+    for start in range(block_size):
+        chunk = encrypted[start:block_size]
+        key += ascii(crack_single_byte_xor(chunk)[0])
+
     return key
 
 
@@ -54,11 +83,14 @@ if __name__ == '__main__':
     print(f'  hamming_dist("{test_a}","{test_b}") = {h}')
     print('  SUCCESS' if (h == test_result) else '  FAILURE')
 
+
     print('Test 2: 6.txt')
     filename = './6.txt'
     with open(filename, 'r') as file:
         encrypted_base64 = ''.join(line.strip() for line in file)
         encrypted_bytearray = base64_to_bytearray(encrypted_base64)
+        print(discover_block_size(encrypted_bytearray))
+
         key = crack_repeating_key_xor(encrypted_bytearray)
         print(f'  detected key = "{key}"')
 
---
+++ RM_A02/single_byte_xor.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+"""
+Decrypt a string XORed against an unknown single byte.
+
+COMP383
+Assignment 1 (Challenge 3)
+
+Ryan Morrell
+"""
+
+from typing import Final, Mapping
+
+from util import bytearray_to_str, hex_to_bytearray
+
+from xor import extend_key, xor
+
+from collections import defaultdict
+
+from scipy.spatial.distance import cosine
+from sklearn.metrics import mean_squared_error
+
+
+_ENGLISH_FREQ: Final[Mapping[str, float]] = {
+    # Frequency table copied from:
+    #    http://www.data-compression.com/english.html
+    'a': 0.0651738,
+    'b': 0.0124248,
+    'c': 0.0217339,
+    'd': 0.0349835,
+    'e': 0.1041442,
+    'f': 0.0197881,
+    'g': 0.0158610,
+    'h': 0.0492888,
+    'i': 0.0558094,
+    'j': 0.0009033,
+    'k': 0.0050529,
+    'l': 0.0331490,
+    'm': 0.0202124,
+    'n': 0.0564513,
+    'o': 0.0596302,
+    'p': 0.0137645,
+    'q': 0.0008606,
+    'r': 0.0497563,
+    's': 0.0515760,
+    't': 0.0729357,
+    'u': 0.0225134,
+    'v': 0.0082903,
+    'w': 0.0171272,
+    'x': 0.0013692,
+    'y': 0.0145984,
+    'z': 0.0007836,
+    ' ': 0.1918182
+}
+
+_ENGLISH_VECTOR = [0.1918182, 0.0651738, 0.0124248, 0.0217339, 
+                   0.0349835, 0.1041442, 0.0197881, 0.0158610, 
+                   0.0492888, 0.0558094, 0.0009033, 0.0050529, 
+                   0.0331490, 0.0202124, 0.0564513, 0.0596302, 
+                   0.0137645, 0.0008606, 0.0497563, 0.0515760,
+                   0.0729357, 0.0225134, 0.0082903, 0.0171272,
+                   0.0013692,0.0145984,0.0007836]
+
+_ENGLISH_CHAR_MAPPING = {
+    ' ': 0,
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5,
+    'f': 6,
+    'g': 7,
+    'h': 8,
+    'i': 9,
+    'j': 10,
+    'k': 11,
+    'l': 12,
+    'm': 13,
+    'n': 14,
+    'o': 15,
+    'p': 16,
+    'q': 17,
+    'r': 18,
+    's': 19,
+    't': 20,
+    'u': 21,
+    'v': 22,
+    'w': 23,
+    'x': 24,
+    'y': 25,
+    'z': 26,
+}
+
+
+def char_freq(string: str, character: str) -> float:
+    """Compute the (normalized) frequency of a character in a string."""
+    # simply count the number of occurrences of a given char
+    # and normalize
+    count = 0
+    for c in string:
+        if c == character:
+            count+=1
+    #TODO
+    return count/len(string)
+
+
+def analyze_char_freq(string: str) -> float:
+    """Check the frequency of English characters in a given string."""
+    # forms a vector representing character frequencies and 
+    # then calculates the cosine similarity between them
+    counts = [0.0]*27
+    for c in string:
+        if c in _ENGLISH_FREQ.keys():
+            counts[_ENGLISH_CHAR_MAPPING[c]]+=(1/len(string))
+    return cosine(_ENGLISH_VECTOR, counts)
+
+
+def crack_single_byte_xor(array: bytearray) -> \
+        tuple[int, str, float]:
+    """Find the single-byte key most likely to yield English text."""
+    best = (0, "", 1.0)
+    for key in range(256):
+        cipher = extend_key(key, len(array))
+        shiftedCiphertext = xor(array, cipher)
+        text = bytearray_to_str(shiftedCiphertext)
+        if text.isprintable() and text != '':
+            score = analyze_char_freq(text)
+            if text.isprintable() and score < best[2]:
+                best = (key, text, score)
+        
+
+    return best
+
+
+if __name__ == '__main__':
+
+    crypttext = '3d1b1c0b02174e17011b4e0d0f00491a4e0c0b4e1d0b1c0701' + \
+        '1b1d514e274e0f034e1d0b1c07011b1d404e2f000a4e0a0100491a4e0d0f' + \
+        '02024e030b4e3d06071c020b17'
+    crypttext_array = hex_to_bytearray(crypttext)
+
+    k, s, e = crack_single_byte_xor(crypttext_array)
+    print(f"best key:       '{ascii(k)}' ({k})  error = {e}")
+    print(f"decoded string: '{s}'")
