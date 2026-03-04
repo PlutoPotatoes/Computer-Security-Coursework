7ce2753f661be3c38bcb6b7c922ddacc484ee63b
{PlutoPotatoes} <{ryan.morrell@gmail.com}>
2026-03-04 10:36:28 -0800

    started A08

---
+++ RM_A08/aes_ctr.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+"""
+AES in CTR mode.
+
+COMP 383
+Assignment 6
+
+Chris Cianci
+"""
+
+
+from aes_ecb import encrypt_aes_128_ecb, random_key
+
+from RM_A07.util import base64_to_bytearray, str_to_bytearray
+
+from RM_A07.xor import xor
+
+
+def aes_128_ctr(data: bytearray,
+                key: bytearray,
+                nonce: int) -> bytearray:
+    """Generate a CTR keystream and XOR against the input.
+
+    Note that 'encryption' and 'decryption' are the *same* operation!
+    """
+    # initialize the counter and the keystream
+    ctr = 0
+    keystream = bytearray()
+    num_blocks = len(data)//16+1
+    first = int.to_bytes(nonce, length=8, byteorder='little')
+    #TODO: Generate the keystream
+    #       Hint--look at the python documentation for int.to_bytes()
+    for i in range(num_blocks):
+        block = bytearray()
+        ctr +=1
+        second = int.to_bytes(ctr, length=8, byteorder='little')
+        block.extend(first)
+        block.extend(second)
+        block = encrypt_aes_128_ecb(block, key)
+        keystream.extend(block)
+
+
+    # XOR the data with the keystream
+
+    return xor(data, keystream[:len(data)])
+
+
+if __name__ == '__main__':
+
+    crypttext = base64_to_bytearray(
+            'ML2iO9/LMpvDyW8yHGOqBqvMCrDxHRiKp78//sCbAH0Nz+DrZR' +
+            'NSa4610kTbifftsvTZXDnwOj20bBUTuwbVkPLjrVnLZzycJdn5' +
+            'h8el+4CszPS9rFx33vv7M2XvekRe/QjhCK5jmdNHUwM3178+G/' +
+            '7g4FZEviHoHZdwtz8='
+        )
+    key = str_to_bytearray('YELLOW SUBMARINE')
+    nonce = 0
+    print(aes_128_ctr(crypttext, key, nonce))
+
+    rk = random_key()
+    rn = 999
+    print(aes_128_ctr(aes_128_ctr(bytearray(
+            b'Possible, probable, my black hen\n' +
+            b'She lays eggs in the relative when\n' +
+            b'She doesn\'t lay eggs in the positive now\n' +
+            b'Because she\'s unable to postulate how\n'
+            ), rk, rn
+        ), rk, rn))
---
+++ RM_A08/random_access_ctr.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+"""
+Random access CTR mode.
+
+COMP 383
+Assignment 8
+
+Chris Cianci
+"""
+
+
+from typing import Callable, Tuple
+
+from aes_ctr import aes_128_ctr
+
+from aes_ecb import random_key
+
+from util import base64_to_bytearray, bytearray_to_str
+
+
+def build_edit_ctr() -> \
+        Tuple[Callable[[bytearray], bytearray],
+              Callable[[bytearray, int, bytearray | bytes], bytearray]]:
+    """Generate a function with 'static' variables."""
+    unknown_key = random_key()
+    unknown_nonce = 0
+
+    def encrypt(clear: bytearray) -> bytearray:
+        encrypted = aes_128_ctr(clear, unknown_key, unknown_nonce)
+        return encrypted
+
+    def edit_ctr(ciphertext: bytearray,
+                 offset: int,
+                 newtext: bytearray | bytes) -> bytearray:
+        """Seek into CTR encrypted data and make a change."""
+        clear = aes_128_ctr(ciphertext, unknown_key, unknown_nonce)
+        # TODO: modify the cleartext before re-encrypting.
+        #       overwrite with "newtext" starting at "offset".
+        encrypted = aes_128_ctr(clear, unknown_key, unknown_nonce)
+        return encrypted
+    return encrypt, edit_ctr
+
+
+def break_random_access_ctr(
+        fn: Callable[[bytearray, int, bytearray | bytes], bytearray],
+        ciphertext: bytearray) -> bytearray:
+    """(Ab)Use edit function to decipher CTR."""
+    cleartext = bytearray(len(ciphertext))
+    # TODO: decipher the ciphertext
+    return cleartext
+
+
+if __name__ == '__main__':
+
+    # construct target functions
+    encrypt_ctr, edit_ctr = build_edit_ctr()
+
+    # prepare (and provide) the original ciphertext
+    filename = './25.txt'
+    encrypted = bytearray()
+    with open(filename, 'r') as file:
+        base64 = ''.join(file.readlines())
+        clear = base64_to_bytearray(base64)
+        encrypted = encrypt_ctr(clear)
+
+    # attack the edit function to recover the plaintext
+    plaintext = break_random_access_ctr(edit_ctr, encrypted)
+    print(bytearray_to_str(plaintext))
